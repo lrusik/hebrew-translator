370ef15988738fc9d1e2a9d2d4605fa6c3243f37
Ruslan Yakushev <yakushev.rusl101@gmail.com>
2020-03-03 11:38:46 +0200

    deploy

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import './css/App.css';
-import Header from "./Header";
-import Footer from "./Footer";
-import Main from "./Main";
+import React, { Component } from 'react'
+import './css/App.css'
+import Header from "./Header"
+import Footer from "./Footer"
+import Main from "./Main"
 
 //nikdat
 class App extends Component {
